133c59dfb7935f9b2ac84b43464fe3f0bf204461
i-sifat <murshedalam850@gmail.com>
2025-01-21 17:11:11 +0600

    fixing app crash

--- android/app/src/main/kotlin/com/example/passify/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/passify/MainActivity.kt
@@ -1,5 +1,18 @@
-package com.example.passify
+package com.android.passifyfree
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.multidex.MultiDex
+import android.content.Context
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
